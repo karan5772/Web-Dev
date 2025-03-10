c219bfaa42bdc2908048e3de21a4b8e39217fdc9
karan5772 <karankumar8239@gmail.com>
2025-03-10 20:50:27 +0530

    js_clockProject

---
+++ js/01_Project/script.js
@@ -0,0 +1,30 @@
+function setdate(){
+    const time = document.getElementById("time");
+    const date = document.getElementById("date");
+    const now = new Date();
+    
+    const hours = now.getHours()%12 || 12; //if it's 12 then it'll retirn 12
+    const min = now.getMinutes().toString().padStart(2,"0");
+    const sec = now.getSeconds().toString().padStart(2,"0");
+    const ampm = now.getHours() >= 12 ? 'PM' : 'AM'; 
+    time.textContent = `${hours}:${min}:${sec} ${ampm}`;
+
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+    
+    const dateString = now.toLocaleDateString(undefined, options);
+    //const dateString = `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
+    date.textContent = dateString;
+
+
+    
+}
+
+
+
+setInterval(setdate, 1000)
+setdate()
